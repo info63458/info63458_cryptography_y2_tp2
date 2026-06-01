5c2a6485955dabe59ec13d418f6de5bc58ffa991
Nozomu Ai <noah.fournis@gmail.com>
2026-06-01 14:13:27 +0200

    started isPrime function to see if number is prime

--- tp2.py
+++ tp2.py
@@ -1,1 +1,14 @@
 #start
+from math import *
+
+def isPrime(n):
+    n=int(input())
+    for i in range(math.sqrt(n)):
+        if n%i == 0 :
+            return False
+        else :
+            return True
+    print(isPrime)
+
+# def RSA_encryption(p,q,m):
+
