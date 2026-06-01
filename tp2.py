4ae752e3a80d042227cc3030a630e6944cd3e39a
Nozomu Ai <noah.fournis@gmail.com>
2026-06-01 14:54:19 +0200

    added GCD function from the TP pdf, works

--- tp2.py
+++ tp2.py
@@ -1,22 +1,28 @@
 #start
 from math import *
 
-def isPrime(n):
-    for i in range(2,int(sqrt(n))+1):
-        if n%i == 0 :
+def isPrime(y):
+    for i in range(2,int(sqrt(y))+1):
+        if y%i == 0 :
             return False
     return True
 
+def GCD(a, b): #Returns the GCD of a and b (Euclidean algorithm).
+    while b != 0:
+        a, b = b, a % b #swap
+    return abs(a) #return absolute value of a
+#taken from the TP pdf
 
-#print(isPrime(9))
-#print(isPrime(7))
+print(GCD(84,126)) #test works
 
 def RSA_encryption(p,q,m):
     # STOP USING INPUT GODDAMNIT
     if isPrime(p) and isPrime(q) :
         print("The numbers are prime")
+        n=p*q
+        phi=(p-1)*(q-1)
     else : 
         print("Please choose prime numbers")
     return #returns None but it isn't printed, unnecessary as it doesn't return a value
 
-RSA_encryption(9,7,11)
+RSA_encryption(23,29,8)
