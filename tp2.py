28ea391d89213d6081b40dd256c358bd683d7e36
Nozomu Ai <noah.fournis@gmail.com>
2026-06-01 14:28:08 +0200

    fixed logic mistakes and invalid syntax, isPrime() is functional

--- tp2.py
+++ tp2.py
@@ -2,13 +2,14 @@
 from math import *
 
 def isPrime(n):
-    n=int(input())
-    for i in range(math.sqrt(n)):
+    for i in range(2,int(sqrt(n))+1):
         if n%i == 0 :
             return False
-        else :
-            return True
-    print(isPrime)
+    return True
+
+
+print(isPrime(9))
+print(isPrime(7))
 
 # def RSA_encryption(p,q,m):
 
